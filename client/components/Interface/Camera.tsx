ed1033f8977b4870f265fe1e5297d56230fb3b4f
Carlos Baraza Haro <carlos.baraza@ieee.org>
2021-01-31 16:34:14 +0000

    Change the camera feed

--- client/components/Interface/Camera.tsx
+++ client/components/Interface/Camera.tsx
@@ -1,22 +1,40 @@
-import React, { FC } from "react";
-import { ACTUAL_ROBOT_API_URL } from "../../lib/config";
+import Head from "next/head";
+import React, { FC, useEffect, useRef } from "react";
+import { ACTUAL_ROBOT_API_URL, ROBOT_CAMERA_FEED } from "../../lib/config";
 
 type Props = {};
 
 export const Camera: FC<Props> = (props) => {
+  const canvasEl = useRef<HTMLCanvasElement>(null);
+  const player = useRef<any>(null);
+
+  useEffect(() => {
+    if (!canvasEl.current) return;
+    player.current = new JSMpeg.Player(ROBOT_CAMERA_FEED, { canvas: canvasEl.current });
+  }, [canvasEl]);
+
   return (
     <>
+      <Head>
+        <script type="text/javascript" src="/js/jsmpeg.min.js"></script>
+      </Head>
+
       <div className="Camera">
-        <img src={`${ACTUAL_ROBOT_API_URL}/camera`} />
+        <canvas id="video-canvas" ref={canvasEl} />
       </div>
 
       <style jsx>{`
         .Camera {
-        }
-
-        img {
+          position: relative;
+          overflow: hidden;
           width: 100%;
           height: 100%;
+        }
+
+        canvas {
+          position: absolute;
+          width: 101%;
+          height: 101%;
         }
       `}</style>
     </>
--- client/lib/config.ts
+++ client/lib/config.ts
@@ -1,3 +1,5 @@
 export const ACTUAL_ROBOT_API_URL = "http://192.168.1.247:2204";
 export const ROBOT_API_URL =
   typeof window === "undefined" ? ACTUAL_ROBOT_API_URL : `http://${window.location.hostname}:2204`;
+
+export const ROBOT_CAMERA_FEED = "ws://192.168.1.247:8082/";
